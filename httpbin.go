b32127b1ae1fbe9c57ca0deb4c5c5f94a944e335
Luo Yuhao <luoyuhao@haujilo.xyz>
2019-05-16 19:25:08 +0800

    feat: Create request headers inspection API.
    re #1

--- httpbin.go
+++ httpbin.go
@@ -1,4 +1,19 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
+	addr := "0.0.0.0:1121"
+	if len(os.Args) > 1 {
+		addr = os.Args[1]
+	}
+	mux := http.NewServeMux()
+	route(mux)
+
+	log.Println("Starting httpbin", addr)
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
