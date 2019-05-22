10171966ae4865d36c3c92c338447b356d0b5566
Luo Yuhao <luoyuhao@haujilo.xyz>
2019-05-22 18:15:32 +0800

    test: Test HTTP Method POST API.
    re #3

--- api/methods_test.go
+++ api/methods_test.go
@@ -1,10 +1,15 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -73,6 +78,129 @@ func TestGETHandler(t *testing.T) {
 			if tt.result.URL != body.URL {
 				t.Errorf("handler returned wrong response json body: got %v want %v",
 					body.URL, tt.result.URL)
+			}
+		})
+	}
+}
+
+func TestPOSTHandler(t *testing.T) {
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+	createTestCase := func(host, path, addr string, headers [][2]string, body io.Reader) args {
+		r, err := http.NewRequest("POST", path, body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		r.RequestURI = path
+		r.Host = host
+		r.RemoteAddr = addr
+		for _, item := range headers {
+			r.Header.Add(item[0], item[1])
+		}
+		return args{httptest.NewRecorder(), r}
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result responsePOSTHandler
+	}{
+		{
+			"TestPOSTHandler1",
+			createTestCase("localhost:1121", "/post?a=1&a=2&b=3", "127.0.0.1:1121", [][2]string{{"Content-Type", "application/x-www-form-urlencoded"}, {"X-TEST", "Test"}}, func() io.Reader {
+				data := url.Values{}
+				data.Add("test1", "test1")
+				data.Add("test1", "test1-1")
+				data.Add("test2", "test2")
+				return strings.NewReader(data.Encode())
+			}()),
+			responsePOSTHandler{
+				Args: map[string]interface{}{
+					"a": []interface{}{"1", "2"},
+					"b": "3",
+				},
+				Headers: map[string]string{"Content-Type": "application/x-www-form-urlencoded", "X-Test": "Test"},
+				Origin:  "127.0.0.1",
+				URL:     "http://localhost:1121/post?a=1\u0026a=2\u0026b=3",
+				Form: map[string]interface{}{
+					"test1": []interface{}{"test1", "test1-1"},
+					"test2": "test2",
+				},
+			},
+		},
+		{
+			"TestPOSTHandler2",
+			func() args {
+				var body bytes.Buffer
+				w := multipart.NewWriter(&body)
+				defer w.Close()
+				w.SetBoundary("1eecdfc9a2d50fa834599aad3e2fb433a5a87887435d5e026e1d651da96a")
+				w.WriteField("test1", "test1")
+				w.WriteField("test1", "test1-1")
+				w.WriteField("test2", "test2")
+				ww, _ := w.CreateFormFile("testfile", "testfile")
+				ww.Write([]byte("abc\n"))
+				return createTestCase("localhost:1121", "/post?a=1&a=2&b=3", "127.0.0.1:1121", [][2]string{{"Content-Type", w.FormDataContentType()}, {"X-TEST", "Test"}}, &body)
+			}(),
+			responsePOSTHandler{
+				Args: map[string]interface{}{
+					"a": []interface{}{"1", "2"},
+					"b": "3",
+				},
+				Headers: map[string]string{"Content-Type": "multipart/form-data; boundary=1eecdfc9a2d50fa834599aad3e2fb433a5a87887435d5e026e1d651da96a", "X-Test": "Test"},
+				Origin:  "127.0.0.1",
+				URL:     "http://localhost:1121/post?a=1\u0026a=2\u0026b=3",
+				Form: map[string]interface{}{
+					"test1": []interface{}{"test1", "test1-1"},
+					"test2": "test2",
+				},
+				Files: map[string]interface{}{
+					"testfile": "abc\n",
+				},
+			},
+		},
+		{
+			"TestPOSTHandler3",
+			createTestCase("localhost:1121", "/post?a=1&a=2&b=3", "127.0.0.1:1121", [][2]string{{"Content-Type", "application/json"}, {"X-TEST", "Test"}}, strings.NewReader("{\"a\": \"1\"}")),
+			responsePOSTHandler{
+				Args: map[string]interface{}{
+					"a": []interface{}{"1", "2"},
+					"b": "3",
+				},
+				Headers: map[string]string{"Content-Type": "application/json", "X-Test": "Test"},
+				Origin:  "127.0.0.1",
+				URL:     "http://localhost:1121/post?a=1\u0026a=2\u0026b=3",
+				JSON:    map[string]interface{}{"a": "1"},
+			},
+		},
+		{
+			"TestPOSTHandler4",
+			createTestCase("localhost:1121", "/post?a=1&a=2&b=3", "127.0.0.1:1121", [][2]string{{"X-TEST", "Test"}}, strings.NewReader("abcdefgh")),
+			responsePOSTHandler{
+				Args: map[string]interface{}{
+					"a": []interface{}{"1", "2"},
+					"b": "3",
+				},
+				Headers: map[string]string{"X-Test": "Test"},
+				Origin:  "127.0.0.1",
+				URL:     "http://localhost:1121/post?a=1\u0026a=2\u0026b=3",
+				Data:    "abcdefgh",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			POSTHandler(tt.args.w, tt.args.r)
+			if status := tt.args.w.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			var body responsePOSTHandler
+			json.Unmarshal(tt.args.w.Body.Bytes(), &body)
+			if !reflect.DeepEqual(tt.result, body) {
+				t.Errorf("handler returned wrong response json body: got %v want %v",
+					body, tt.result)
 			}
 		})
 	}
