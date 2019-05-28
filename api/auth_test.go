acf4ffdd89ddc0c2351fe5eed0ac7644b1aad947
Luo Yuhao <luoyuhao@haujilo.xyz>
2019-05-28 17:10:46 +0800

    test: Test Bearer Auth API.
    re #13

--- api/auth_test.go
+++ api/auth_test.go
@@ -54,3 +54,49 @@ func TestBasicAuthHander(t *testing.T) {
 		})
 	}
 }
+
+func TestBearerAuthHander(t *testing.T) {
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+	createTestCase := func(token string) args {
+		r, err := http.NewRequest("GET", "/bearer", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if token != "" {
+			r.Header.Set("Authorization", "Bearer "+token)
+		}
+		return args{httptest.NewRecorder(), r}
+	}
+	type result struct {
+		code     int
+		response *bearerAuthJSONResponse
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result result
+	}{
+		{"TestBearerAuthHander1", createTestCase("mF_9.B5f-4.1JqM"), result{200, &bearerAuthJSONResponse{true, "mF_9.B5f-4.1JqM"}}},
+		{"TestBearerAuthHander2", createTestCase(""), result{401, nil}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			BearerAuthHander(tt.args.w, tt.args.r)
+			if status := tt.args.w.Code; status != tt.result.code {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, tt.result.code)
+			}
+			if tt.result.response != nil {
+				var body bearerAuthJSONResponse
+				json.Unmarshal(tt.args.w.Body.Bytes(), &body)
+				if !reflect.DeepEqual(*tt.result.response, body) {
+					t.Errorf("handler returned wrong response json body: got %v want %v",
+						body, *tt.result.response)
+				}
+			}
+		})
+	}
+}
