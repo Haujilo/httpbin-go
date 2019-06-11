599d3b7f4c7bfe6055791800e5e6d6e1e8cb73ec
Luo Yuhao <luoyuhao@haujilo.xyz>
2019-06-11 18:02:47 +0800

    test: Test Deflate API.
    re #23

--- api/formatresp_test.go
+++ api/formatresp_test.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"compress/flate"
 	"compress/gzip"
 	"encoding/json"
 	"io/ioutil"
@@ -203,6 +204,56 @@ func TestGZipHandler(t *testing.T) {
 				t.Error(err)
 			}
 			var body gzipJSONResponse
+			json.Unmarshal(content, &body)
+			if !reflect.DeepEqual(tt.result, body) {
+				t.Errorf("handler returned wrong response json body: got %v want %v",
+					body, tt.result)
+			}
+		})
+	}
+}
+
+func TestDeflateHandler(t *testing.T) {
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+	createTestCase := func(name string) struct {
+		name   string
+		args   args
+		result deflateJSONResponse
+	} {
+		r, err := http.NewRequest("GET", "/deflate", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return struct {
+			name   string
+			args   args
+			result deflateJSONResponse
+		}{name, args{httptest.NewRecorder(), r}, deflateJSONResponse{true, fmtHeaders(r), r.Method, getIP(r)}}
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result deflateJSONResponse
+	}{
+		createTestCase("TestDeflateHandler1"),
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DeflateHandler(tt.args.w, tt.args.r)
+			if status := tt.args.w.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			reader := flate.NewReader(bytes.NewReader(tt.args.w.Body.Bytes()))
+			defer reader.Close()
+			content, err := ioutil.ReadAll(reader)
+			if err != nil {
+				t.Error(err)
+			}
+			var body deflateJSONResponse
 			json.Unmarshal(content, &body)
 			if !reflect.DeepEqual(tt.result, body) {
 				t.Errorf("handler returned wrong response json body: got %v want %v",
