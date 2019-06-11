c41e4f15abccfecc9725a0c4716250e78fb2d7fd
Luo Yuhao <luoyuhao@haujilo.xyz>
2019-06-11 17:25:59 +0800

    test: Test GZip API.
    re #23

--- api/formatresp_test.go
+++ api/formatresp_test.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
@@ -150,6 +154,59 @@ func TestHTMLHandler(t *testing.T) {
 			}
 			if string(body) != tt.result {
 				t.Errorf("http body error, got %v want %v", tt.result, body)
+			}
+		})
+	}
+}
+
+func TestGZipHandler(t *testing.T) {
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+	createTestCase := func(name string) struct {
+		name   string
+		args   args
+		result gzipJSONResponse
+	} {
+		r, err := http.NewRequest("GET", "/gzip", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return struct {
+			name   string
+			args   args
+			result gzipJSONResponse
+		}{name, args{httptest.NewRecorder(), r}, gzipJSONResponse{true, fmtHeaders(r), r.Method, getIP(r)}}
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result gzipJSONResponse
+	}{
+		createTestCase("TestHTMLHandler1"),
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GZipHandler(tt.args.w, tt.args.r)
+			if status := tt.args.w.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			reader, err := gzip.NewReader(bytes.NewReader(tt.args.w.Body.Bytes()))
+			defer reader.Close()
+			if err != nil {
+				t.Error(err)
+			}
+			content, err := ioutil.ReadAll(reader)
+			if err != nil {
+				t.Error(err)
+			}
+			var body gzipJSONResponse
+			json.Unmarshal(content, &body)
+			if !reflect.DeepEqual(tt.result, body) {
+				t.Errorf("handler returned wrong response json body: got %v want %v",
+					body, tt.result)
 			}
 		})
 	}
