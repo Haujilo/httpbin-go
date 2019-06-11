4550624ee8899381eb51d258e0f797a2119d4f73
Luo Yuhao <luoyuhao@haujilo.xyz>
2019-06-11 14:57:50 +0800

    test: Test XML API.
    re #23

--- api/formatresp_test.go
+++ api/formatresp_test.go
@@ -80,3 +80,40 @@ func TestJsonHandler(t *testing.T) {
 		})
 	}
 }
+
+func TestXMLHandler(t *testing.T) {
+	type args struct {
+		w *httptest.ResponseRecorder
+		r *http.Request
+	}
+	createTestCase := func() args {
+		r, err := http.NewRequest("GET", "/json", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		return args{httptest.NewRecorder(), r}
+	}
+	tests := []struct {
+		name   string
+		args   args
+		result string
+	}{
+		{"TestXMLHandler1", createTestCase(), xmlBody},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			XMLHandler(tt.args.w, tt.args.r)
+			if status := tt.args.w.Code; status != http.StatusOK {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, http.StatusOK)
+			}
+			body, err := ioutil.ReadAll(tt.args.w.Body)
+			if err != nil {
+				t.Error(err)
+			}
+			if string(body) != tt.result {
+				t.Errorf("http body error, got %v want %v", tt.result, body)
+			}
+		})
+	}
+}
